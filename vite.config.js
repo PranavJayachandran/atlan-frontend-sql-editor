d3570b3443d36129de389f37123c54394528556f
PranavJayachandran <pranavjay02@gmail.com>
2023-09-18 03:01:04 +0530

    Removed pwa from config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dsv from '@rollup/plugin-dsv'
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react(), dsv(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [react(), dsv()]
 })
 
