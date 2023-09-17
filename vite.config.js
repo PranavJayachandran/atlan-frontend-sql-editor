c1ffe9db3366f4b8b270777fb2ad9508ae8c97d8
PranavJayachandran <pranavjay02@gmail.com>
2023-09-18 02:56:13 +0530

    Added the pwa config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dsv from '@rollup/plugin-dsv'
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dsv()],
+  plugins: [react(), dsv(), VitePWA({ registerType: 'autoUpdate' })],
 })
+
